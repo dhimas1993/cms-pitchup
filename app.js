317196483f421a673dc7f2ccac5487f65647275c
hartatoadi <dhimashertianto@gmail.com>
2021-03-15 12:31:49 +0700

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose =require('mongoose')
 require('dotenv').config();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,7 +21,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
